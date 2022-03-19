c62affee881636abd96cb225a892c65b9c422224
Bekzod <bekzodmirsaidov91@gmail.com>
2022-03-19 17:39:04 +0500

    7-misol

--- if07.py
+++ if07.py
@@ -1,11 +1,11 @@
 def main(a):
-    if a%2>0:
+    if a%2==0 and a>0:
         return "positive odd number"
-    if (a+1)%2>0:
+    elif a>0 and a%2==1:
         return "positive even number"
-    if a%2<0:
+    elif a%2==0 and a<0:
         return "negative odd number"
-    if (a+1)%2<0:
+    elif a%2==1 and a<0:
         return "negative even number"
     else:
         return "the number is zero"
