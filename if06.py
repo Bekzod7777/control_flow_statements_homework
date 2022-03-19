7bc7e62eafe27faaee2fc548aefa722665d0a732
Bekzod <bekzodmirsaidov91@gmail.com>
2022-03-19 16:16:56 +0500

    6-misol

--- if06.py
+++ if06.py
@@ -1,4 +1,21 @@
 def main(a,b,c):
+    s=0
+    k=0
+    if a>0:
+        s=s+1
+    if b>0:
+        s=s+1
+    if c>0:
+        s=s+1
+    if a<0:
+        k=k+1
+    if b<0:
+        k=k+1
+    if c<0:
+        k=k+1
+    return s,k
+    
+    
     """
     Find how many positive and how many negative numbers there are in the given numbers.
     check the following conditions:
@@ -13,4 +30,4 @@ def main(a,b,c):
     Returns:
         string: string with the result
     """
-    return
+    
