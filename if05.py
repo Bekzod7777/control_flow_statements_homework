8f24005a7534ea729449f591f24f045c9c061ab8
Bekzod <bekzodmirsaidov91@gmail.com>
2022-03-19 16:10:25 +0500

    5-misol

--- if05.py
+++ if05.py
@@ -1,4 +1,13 @@
 def main(a,b,c):
+    s=0
+    if a<0:
+        s=s+1
+    if b<0:
+        s=s+1
+    if c<0:
+        s=s+1
+    return s
+
     """
     Find how many negative numbers there are in the given numbers.
     Args:
@@ -8,4 +17,4 @@ def main(a,b,c):
     returns:
         integer: the number of negative numbers in the given numbers
     """
-    return
+   
