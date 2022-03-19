4439e07e97b60124d702d571be78a655511e74e3
Bekzod <bekzodmirsaidov91@gmail.com>
2022-03-19 17:51:17 +0500

    7-misol

--- if07.py
+++ if07.py
@@ -1,14 +1,15 @@
 def main(a):
     if a%2==0 and a>0:
-        return "positive odd number"
-    elif a%2==1 and  a>0 :
         return "positive even number"
+    elif a>0 and a%2==1:
+        return "positive odd number"
     elif a%2==0 and a<0:
-        return "negative odd number"
-    elif a%2==1 and a<0:
         return "negative even number"
+    elif a%2==1 and a<0:
+        return "negative odd number"
     else:
         return "the number is zero"
+
     
 """
     Given an integer a, check the following conditions:
