5ca85d8468a585f7ca787c995733b303dd6857aa
Bekzod <bekzodmirsaidov91@gmail.com>
2022-03-19 15:44:42 +0500

    4-misol

--- if04.py
+++ if04.py
@@ -1,4 +1,16 @@
+from re import S
+
+
 def main(a,b,c):
+    s=0
+    if a>0:
+        return s+1
+    elif b>0:
+        return s+1
+    elif c>0:
+        return s+1
+    else:
+        return s
     """
     Find how many positive numbers there are in the given numbers.
     Args:
@@ -8,4 +20,4 @@ def main(a,b,c):
     returns:
         integer: the number of positive numbers in the given numbers
     """
-    return
+    
